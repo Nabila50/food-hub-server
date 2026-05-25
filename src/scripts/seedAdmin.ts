7477db923c5b3aa91d2d86b9946ebcc23809e7da
Nabila <nabila.nawshin50@gmail.com>
2026-05-26 01:37:22 +0200

    admin seeding is completed

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -5,10 +5,11 @@ async function seedAdmin(){
     try{
 
         const adminData = {
-            name: "Admin Saheb",
-            email: "admin@admin.com",
+            name: "Admin Bro",
+            email: "admin@gmail.com",
             role: UserRole.ADMIN,
-            password:"admin1234"
+            password:"admin1234",
+            image: "https://i.ibb.co/s9p8VXgS/women-Photo.jpg"
         }
         // check user exist on db or not?
         const existingUser = await prisma.user.findUnique({
@@ -22,17 +23,39 @@ async function seedAdmin(){
             throw new Error("User already exists in DB");
         }
 
-        const signUpAdmin = await fetch("http://localhost:5000/api/auth/sign-up/email", {
+        // ^ signup as an Admin/ create Admin
+         const signUpAdmin = await fetch("http://localhost:5000/api/auth/sign-up/email", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-
+                "Origin": "http://localhost:5000"
             },
             body: JSON.stringify(adminData)
         })
+        console.log(signUpAdmin)
 
+        //* email verification for admin
+        if(signUpAdmin.ok){
+            console.log("admin creadted")
+            await prisma.user.update({
+                
+                where:{
+                    email: adminData.email
+                },
+                data:{
+                    emailVerified: true
+                }
+               
+            })
+             console.log("admin role updated!!!")
+
+            console.log("_________Email Verified________")
+        };
+ 
 
     }catch(error){
         console.log(error)
     }
 }
+
+seedAdmin();
